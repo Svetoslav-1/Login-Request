197ba62454704c084adc84921dc65e6edda61695
Svetoslav-1 <98308472+Svetoslav-1@users.noreply.github.com>
2023-05-25 21:29:43 +0300

    Create User.h
    Elements of the user.

---
+++ User.h
@@ -0,0 +1,15 @@
+#pragma once
+
+using namespace System;
+using namespace std;
+
+public ref class User {
+public:
+	String^ name;
+	String^ email;
+	String^ phone;
+	String^ address;
+	String^ password;
+  int id;
+
+};
